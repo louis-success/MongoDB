55acec1fc96f3d60fff5d4c4ae3d9abcc6b51e29
Louis Raj Ulaganathan <louisrajulaganathan@Louiss-MacBook-Pro.local>
2021-04-08 16:24:45 -0500

    Lab exercise added

--- FirstMongoDBClientApp/FirstMongoDBClientApp/Program.cs
+++ FirstMongoDBClientApp/FirstMongoDBClientApp/Program.cs
@@ -9,18 +9,26 @@ namespace FirstMongoDBClientApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var mongoClient = new MongoClient("mongodb+srv://m220student:m220password@mflix.gcgrf.mongodb.net/sample_mflix?retryWrites=true&w=majority");
-            var mongoDB = mongoClient.GetDatabase("sample_mflix");
-            //var collections = mongoDB.ListCollections().ToList();
-            //Console.WriteLine("*****Available Collections*****");
-            //foreach (var documentCollection in collections)
-            //{
-            //    Console.WriteLine($"{documentCollection..do}");
-            //}
-            var collection = mongoDB.GetCollection<BsonDocument>("movies");
-            var document = collection.Find("{title: 'Blacksmith Scene'}").FirstOrDefault();
-            Console.WriteLine("*****Search Document*****");
-            Console.Write(document);
+            var connString = "mongodb+srv://m220student:m220password@mflix.gcgrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+            var client = new MongoClient(connString);
+            var db = client.GetDatabase("sample_mflix");
+            var collection = db.GetCollection<BsonDocument>("movies");
+            var result = collection.Find(new BsonDocument())
+               .SortByDescending(m => m["runtime"])
+               .Limit(10)
+               .ToList();
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r.GetValue("title"));
+            }
+
+            //var mongoClient = new MongoClient("mongodb+srv://m220student:m220password@mflix.gcgrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            //var mongoDB = client.GetDatabase("sample_mflix");
+            //var collection = mongoDB.GetCollection<BsonDocument>("movies");
+            //var document = collection.Find("{title: 'The Princess Bride'}").FirstOrDefault();
+            //Console.WriteLine("*****Search Document*****");
+            //Console.Write(document);
 
         }
     }
