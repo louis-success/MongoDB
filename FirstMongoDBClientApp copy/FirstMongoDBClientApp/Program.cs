d91724c5f5b7b19d32874ad962d05c84d4190e2d
Louis Raj Ulaganathan <louisrajulaganathan@Louiss-MacBook-Pro.local>
2021-04-08 15:17:21 -0500

    Project added

---
+++ FirstMongoDBClientApp copy/FirstMongoDBClientApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace FirstMongoDBClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var mongoClient = new MongoClient("mongodb+srv://m220student:m220password@mflix.gcgrf.mongodb.net/sample_mflix?retryWrites=true&w=majority");
+            var mongoDB = mongoClient.GetDatabase("sample_mflix");
+            //var collections = mongoDB.ListCollections().ToList();
+            //Console.WriteLine("*****Available Collections*****");
+            //foreach (var documentCollection in collections)
+            //{
+            //    Console.WriteLine($"{documentCollection..do}");
+            //}
+            var collection = mongoDB.GetCollection<BsonDocument>("movies");
+            var document = collection.Find("{title: 'Blacksmith Scene'}").FirstOrDefault();
+            Console.WriteLine("*****Search Document*****");
+            Console.Write(document);
+
+        }
+    }
+}
